0e33edfd9b75f71713d51f31d9b99092bd05e415
unknown5325 <roprawezava@tutamail.com>
2025-10-03 13:25:42 +0300

    seccond project

--- l3_math.py
+++ l3_math.py
@@ -16,5 +16,42 @@ print(f"Длина стороны вписанного квадрата: {kvad} 
 print(f"Длина стороны вписанного треугольника: {trel} см или {trel/100} м") # Выводим результат
 print(f"Длина стороны описанного треугольника: {trel_OP} см или {trel_OP/100} м") # Выводим результат
 print(f"Длина стороны описанного квадрата: {kvad_OP} см или{kvad_OP/100} м") # Выводим результат
-print(f"Длина стороны описанного восьмигольника: {vasm} см или {vasm/100} м") # Выводим результат
+print(f"Длина стороны описанного восьмиугольника: {vasm} см или {vasm/100} м") # Выводим результат
 
+
+'''
+import math
+
+spic = (input("Введите список: ")).split()
+for i in range(0,len(spic)):
+    spic[i] = int(spic[i])
+ch = int(input("Введите параметр: "))
+
+print(spic)
+sumb = 0
+match ch:
+    case 0:
+            for i in range(0,len(spic)):
+                spic[i] = 0
+            print(spic)
+    case 1:
+            for i in range(0,len(spic)):
+                spic[i] = spic[i]*2
+            print(spic)
+    case 2:
+            for i in range(0,len(spic)):
+                if i%2 == 0:
+                    spic[i]= spic[i] + 10
+            print(spic)
+    case 3:
+            for i in range(0,len(spic), 3):
+                if spic[i] > 0:
+                    spic[i] = math.sqrt(spic[i])
+                else:
+                    spic[i] = 0
+            print(spic)
+    case _:
+            for i in range(0,len(spic)):
+                sumb += spic[i]
+            print(sumb)
+            '''
